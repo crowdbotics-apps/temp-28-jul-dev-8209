ee6e8f36446de7b2e7c220589bd6a1ab071f2031
Crowdbotics <team@crowdbotics.com>
2020-07-28 05:42:14 +0000

    Update components

--- src/features/BlankScreen19428/screens/blank.js
+++ src/features/BlankScreen19428/screens/blank.js
@@ -22,11 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = {
-    Datepicker_11: new Date("07/28/2020"),
-    Toggle_17: true,
-    Datepicker_11: new Date("07/28/2020")
-  }
+  state = {}
 
   render = () => (
     <View
@@ -65,31 +61,23 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <View
+      <Icon
+        iconFont="Eva Design Icons"
+        name="star"
+        width={20}
+        height={20}
         style={{
-          width: "100%",
-          height: "33.333333333333336%",
           marginLeft: 0,
           marginRight: 0,
-          marginTop: 0,
-          marginBottom: 0,
-          paddingLeft: 0,
-          paddingRight: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
           overflow: "visible",
           textAlign: "left",
           verticalAlign: "baseline",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "flex-start",
-          alignItems: "stretch",
-          alignContent: "stretch",
           borderColor: "#000000",
           borderStyle: "solid",
           borderWidth: 0,
@@ -98,386 +86,17 @@ export class _Blank extends React.Component {
           borderTopWidth: 0,
           borderBottomWidth: 0,
           borderRadius: 0,
-          backgroundSize: "auto"
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          textDecorationLine: "none",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
         }}
-      >
-        <View
-          style={{
-            width: "50%",
-            height: "100%",
-            marginLeft: 0,
-            marginRight: 0,
-            marginTop: 0,
-            marginBottom: 0,
-            paddingLeft: 0,
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: 12,
-            color: "#000000",
-            backgroundColor: "#ffffff",
-            fontStyle: "normal",
-            fontWeight: "normal",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderColor: "#000000",
-            borderStyle: "solid",
-            borderWidth: 0,
-            borderLeftWidth: 0,
-            borderRightWidth: 0,
-            borderTopWidth: 0,
-            borderBottomWidth: 0,
-            borderRadius: 0,
-            backgroundSize: "auto"
-          }}
-        >
-          <Button
-            style={{
-              width: "100%",
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "center",
-              verticalAlign: "baseline",
-              fontSize: 12,
-              color: "#000000",
-              backgroundColor: "#3366FF",
-              fontStyle: "normal",
-              fontWeight: "normal",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0,
-              textDecorationLine: "none",
-              textTransform: "none",
-              lineHeight: 12,
-              letterSpacing: 0
-            }}
-            onPress={() => alert("Pressed!")}
-          >
-            Press me!
-          </Button>
-          <Datepicker
-            style={{
-              width: "100%",
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "left",
-              verticalAlign: "baseline",
-              fontSize: 12,
-              color: "#000000",
-              backgroundColor: "#ffffff",
-              fontStyle: "normal",
-              fontWeight: "normal",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0,
-              textDecorationLine: "none",
-              textTransform: "none",
-              lineHeight: 12,
-              letterSpacing: 0
-            }}
-            date={this.state.Datepicker_11}
-            onSelect={nextValue => this.setState({ Datepicker_11: nextValue })}
-          />
-          <Slider
-            value={50}
-            minimumValue={0}
-            maximumValue={100}
-            step={1}
-            disabled={false}
-            style={{
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "left",
-              verticalAlign: "baseline",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0
-            }}
-          />
-        </View>
-        <View
-          style={{
-            width: "50%",
-            height: "100%",
-            marginLeft: 0,
-            marginRight: 0,
-            marginTop: 0,
-            marginBottom: 0,
-            paddingLeft: 0,
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: 12,
-            color: "#000000",
-            backgroundColor: "#ffffff",
-            fontStyle: "normal",
-            fontWeight: "normal",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderColor: "#000000",
-            borderStyle: "solid",
-            borderWidth: 0,
-            borderLeftWidth: 0,
-            borderRightWidth: 0,
-            borderTopWidth: 0,
-            borderBottomWidth: 0,
-            borderRadius: 0,
-            backgroundSize: "auto"
-          }}
-        >
-          <Toggle
-            text="switch ON/OFF"
-            activeColor="#409EFF"
-            inactiveColor="#C0CCDA"
-            disabled={false}
-            style={{
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "left",
-              verticalAlign: "baseline",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0,
-              textDecorationLine: "none",
-              textTransform: "none",
-              lineHeight: 12,
-              letterSpacing: 0
-            }}
-            checked={this.state.Toggle_17}
-            onChange={nextChecked => this.setState({ Toggle_17: nextChecked })}
-          />
-          <Text
-            style={{
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "left",
-              verticalAlign: "baseline",
-              fontSize: 12,
-              color: "#000000",
-              backgroundColor: "#ffffff",
-              fontStyle: "normal",
-              fontWeight: "normal",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0,
-              textDecorationLine: "none",
-              textTransform: "none",
-              lineHeight: 12,
-              letterSpacing: 0
-            }}
-          >
-            Sample text content
-          </Text>
-        </View>
-        <View
-          style={{
-            width: "50%",
-            height: "100%",
-            marginLeft: 0,
-            marginRight: 0,
-            marginTop: 0,
-            marginBottom: 0,
-            paddingLeft: 0,
-            paddingRight: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: 12,
-            color: "#000000",
-            backgroundColor: "#ffffff",
-            fontStyle: "normal",
-            fontWeight: "normal",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            justifyContent: "flex-start",
-            alignItems: "stretch",
-            alignContent: "stretch",
-            borderColor: "#000000",
-            borderStyle: "solid",
-            borderWidth: 0,
-            borderLeftWidth: 0,
-            borderRightWidth: 0,
-            borderTopWidth: 0,
-            borderBottomWidth: 0,
-            borderRadius: 0,
-            backgroundSize: "auto"
-          }}
-        >
-          <Button
-            style={{
-              width: "100%",
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "center",
-              verticalAlign: "baseline",
-              fontSize: 12,
-              color: "#000000",
-              backgroundColor: "#3366FF",
-              fontStyle: "normal",
-              fontWeight: "normal",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0,
-              textDecorationLine: "none",
-              textTransform: "none",
-              lineHeight: 12,
-              letterSpacing: 0
-            }}
-            onPress={() => alert("Pressed!")}
-          >
-            Press me!
-          </Button>
-          <Datepicker
-            style={{
-              width: "100%",
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "left",
-              verticalAlign: "baseline",
-              fontSize: 12,
-              color: "#000000",
-              backgroundColor: "#ffffff",
-              fontStyle: "normal",
-              fontWeight: "normal",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0,
-              textDecorationLine: "none",
-              textTransform: "none",
-              lineHeight: 12,
-              letterSpacing: 0
-            }}
-            date={this.state.Datepicker_11}
-            onSelect={nextValue => this.setState({ Datepicker_11: nextValue })}
-          />
-          <Slider
-            value={50}
-            minimumValue={0}
-            maximumValue={100}
-            step={1}
-            disabled={false}
-            style={{
-              marginLeft: 0,
-              marginRight: 0,
-              marginTop: 5,
-              marginBottom: 5,
-              paddingLeft: 5,
-              paddingRight: 5,
-              paddingTop: 5,
-              paddingBottom: 5,
-              overflow: "visible",
-              textAlign: "left",
-              verticalAlign: "baseline",
-              borderColor: "#000000",
-              borderStyle: "solid",
-              borderWidth: 0,
-              borderLeftWidth: 0,
-              borderRightWidth: 0,
-              borderTopWidth: 0,
-              borderBottomWidth: 0,
-              borderRadius: 0
-            }}
-          />
-        </View>
-      </View>
+      />
     </View>
   )
 }
